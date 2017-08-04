6bc04b04b0ea46f1a8bc299a32ab34f9e13caed2
qijunjun <2269873327@qq.com>
2017-08-04 13:39:35 +0800

    [新增]首页图片样式js文件

---
+++ js/slide.js
@@ -0,0 +1,53 @@
+/**
+ * Created by 123 on 2017/8/4.
+ */
+$(function(){
+    autoplay();
+    var prev = 0;
+    var $li = $(".slide li");
+    var liLen = $li.length;
+    var timer;
+    function autoplay(){
+        timer =setInterval(function(){
+            prev=$(".points .active").prevAll().length;
+            nowLi = prev+1;
+            $(".points li").eq(nowLi).addClass("active").siblings().removeClass("active");
+            //滑动到最后一张幻灯片
+            if(nowLi > liLen-1){
+                $li.eq(0).css({left:1113}).animate({left:0});
+                $li.eq(prev).animate({left:-1113});
+                $(".points li").eq(0).addClass("active").siblings().removeClass("active");
+            }else{
+                $li.eq(prev).animate({left:-1113});
+                $li.eq(nowLi).css({left:1113}).animate({left:0});
+            }
+        },5000);
+    }
+    $li.hover(function(){
+        clearInterval(timer);
+    },function(){
+        autoplay();
+    });
+    //幻灯片切换
+    function move(prevLi,nowLi){
+        //判断prevLi和nowLi的大小，来确定幻灯片往哪边移动
+        if(prevLi == nowLi){
+            return false;
+        }else if(prevLi<nowLi){
+            //比如所在位置为第1个，此时点击第4个按钮
+            $li.eq(prevLi).animate({left:-1113});
+            $li.eq(nowLi).css({left:1113}).animate({left:0});
+        }else{
+            //比如当前所在的位置为3，此时点击第2个按钮，需要往右滑
+            $li.eq(prevLi).animate({left:1113});
+            $li.eq(nowLi).css({left:-1113}).animate({left:0});
+        }
+    }
+    $(".points").delegate("li","click",function(){
+        prev = $(".points .active").prevAll().length;
+        //即将要滑来的元素
+        var i = $(this).index();
+        move(prev,i);
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+});
